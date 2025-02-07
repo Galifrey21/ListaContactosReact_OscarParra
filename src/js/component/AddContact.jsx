646959f1ccd118fe94bab9a86ebf626663fc68ef
Oscar Parra <oscarparrapalomino@gmail.com>
2025-02-07 11:41:55 +0000

    First Commit

--- src/js/component/AddContact.jsx
+++ src/js/component/AddContact.jsx
@@ -1,30 +1,64 @@
-import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
 
 const API_URL = "https://playground.4geeks.com/contact/";
 const AGENDA_NAME = "test";
 
 const AddContact = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const { contact } = location.state || {};
 
     const [contact_name, setContactName] = useState("");
     const [contact_address, setContactAddress] = useState("");
     const [contact_email, setContactEmail] = useState("");
     const [contact_phone, setContactPhone] = useState("");
 
+    useEffect(() => {
+        if (contact) {
+            setContactName(contact.name);
+            setContactAddress(contact.address);
+            setContactEmail(contact.email);
+            setContactPhone(contact.phone);
+        }
+    }, []);
+
     const add = () => {
-        fetch(API_URL + "agendas/"+AGENDA_NAME+"/contacts",{
-            method:"POST",
-            body:JSON.stringify({
+        fetch(API_URL + "agendas/" + AGENDA_NAME + "/contacts", {
+            method: "POST",
+            body: JSON.stringify({
                 "name": contact_name,
                 "phone": contact_phone,
                 "email": contact_email,
                 "address": contact_address
-              }),
+            }),
             headers: { "Content-Type": "application/json" },
-          } )
+        })
             .then((resp) => resp.json())
             .then((data) => {
-              console.log("Tareas obtenidas:", data);
+                setContactName("");
+                setContactAddress("");
+                setContactEmail("");
+                setContactPhone("");
+            })
+            .catch((error) => console.error("Error al cargar tareas:", error));
+    }
+
+    const update = () => {
+        fetch(API_URL + "agendas/" + AGENDA_NAME + "/contacts/" + contact.id, {
+            method: "PUT",
+            body: JSON.stringify({
+                "name": contact_name,
+                "phone": contact_phone,
+                "email": contact_email,
+                "address": contact_address
+            }),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                navigate('/');
             })
             .catch((error) => console.error("Error al cargar tareas:", error));
     }
@@ -36,11 +70,16 @@ const AddContact = () => {
                     <Link to="/">
                         <span className="btn btn-primary" role="button">Back</span>
                     </Link>
-                    <input className="w-100" name="contact_name" type="text" required placeholder="Nombre" value={contact_name} onChange={(e) => setContactName(e.target.value)}/>
-                    <input className="w-100 mt-3" name="contact_address" type="text" required placeholder="Dirección" value={contact_address} onChange={(e) => setContactAddress(e.target.value)}/>
-                    <input className="w-100 mt-3" name="contact_email" type="text" required placeholder="Email" value={contact_email} onChange={(e) => setContactEmail(e.target.value)}/>
-                    <input className="w-100 mt-3" name="contact_phone" type="text" required placeholder="Telefono"value={contact_phone} onChange={(e) => setContactPhone(e.target.value)} />
-                    <button className="btn btn-success w-100 mt-3" onClick={add}>Añadir</button>
+                    <input className="w-100 mt-3" name="contact_name" type="text" required placeholder="Nombre" value={contact_name} onChange={(e) => setContactName(e.target.value)} />
+                    <input className="w-100 mt-3" name="contact_address" type="text" required placeholder="Dirección" value={contact_address} onChange={(e) => setContactAddress(e.target.value)} />
+                    <input className="w-100 mt-3" name="contact_email" type="text" required placeholder="Email" value={contact_email} onChange={(e) => setContactEmail(e.target.value)} />
+                    <input className="w-100 mt-3" name="contact_phone" type="text" required placeholder="Telefono" value={contact_phone} onChange={(e) => setContactPhone(e.target.value)} />
+                    {contact
+                        ?
+                        <button className="btn btn-success w-100 mt-3" onClick={update}>Guardar</button>
+                        :
+                        <button className="btn btn-success w-100 mt-3" onClick={add}>Añadir</button>
+                    }
                 </div>
             </div>
         </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,16 +1,108 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import imagen from "../../img/rigoBaby.jpg"
 import "../../styles/home.css";
 
-export const Home = () => (
-<div className="container mt-5">
-<div className="row justify-content-end">
-<div className="col-2">
-<Link to="/addcontact">
-<span className="btn btn-primary" role="button">AÑADIR</span>
-</Link>
-</div>
-</div>
-</div>
+const API_URL = "https://playground.4geeks.com/contact/";
+const AGENDA_NAME = "test";
 
-);
+export const Home = () => {
+	const navigate = useNavigate();
+
+	const [agenda, setAgenda] = useState({});
+
+	useEffect(() => {
+		//Recuperamos agenda
+		fetch(API_URL + "agendas/" + AGENDA_NAME, {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((resp) => {
+				//Si agenda no existe, la creamos
+				if (resp.statusText === "Not Found") {
+					createAgenda();
+				} else {
+					return resp.json();
+				}
+			})
+			.then((data) => {
+				//Guardar informacion agenda
+				setAgenda(data);
+			})
+			.catch((error) => console.error("Error al cargar agendas:", error));
+	}, []);
+
+	const createAgenda = () => {
+		fetch(API_URL + "agendas/" + AGENDA_NAME, {
+			method: "POST",
+			body: "",
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((resp_create) => resp_create.json())
+			.then((data_create) => {
+				//Guardar informacion agenda
+				setAgenda(data_create);
+			})
+			.catch((error) => console.error("Error al crear agenda:", error));
+	}
+
+	const deleteContact = (id_contact) => {
+		if (confirm('Seguro que quieres elimar?')) {
+			fetch(API_URL + "agendas/" + AGENDA_NAME + "/contacts/" + id_contact, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+			})
+				.then((resp) => {
+					const _agenda = { ...agenda };
+					const index_contact = _agenda.contacts.findIndex(x => x.id === id_contact);
+					if (index_contact != -1) {
+						_agenda.contacts.splice(index_contact, 1);
+						setAgenda(_agenda);
+					}
+				})
+				.catch((error) => console.error("Error al crear agenda:", error));
+		}
+	}
+
+	const updateContact = (contact) => {
+		navigate('/addcontact', { state: { contact: contact } });
+	}
+
+	return (
+		<div className="container mt-5">
+			<div className="row justify-content-end">
+				<div className="col-2">
+					<Link to="/addcontact">
+						<span className="btn btn-primary" role="button">Añadir</span>
+					</Link>
+				</div>
+			</div>
+			<div className="container d-flex flex-column align-items-center">
+				{agenda.contacts && agenda.contacts.map((contact, index) => (
+					<div key={index} className="card mb-3" style={{ maxWidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src={imagen} className="img-fluid rounded-start" alt="..." />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body d-flex justify-content-between">
+									<div>
+										<h5 className="card-title">{contact.name}</h5>
+										<p className="card-text"><strong className="me-2">Dirección:</strong>{contact.address}</p>
+										<p className="card-text"><strong className="me-2">Teléfono:</strong>{contact.phone}</p>
+										<p className="card-text"><strong className="me-2">Email:</strong>{contact.email}</p>
+									</div>
+									<div>
+										<button type="button" className="btn btn-primary me-1" onClick={() => updateContact(contact)}>Editar</button>
+										<button type="button" className="btn btn-danger ms-1" onClick={() => deleteContact(contact.id)}>Eliminar</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+
+	);
+};
